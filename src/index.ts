454058cc533183b11cbef4c6f9138e62fd5c19b5
Lucas Wilson <lucaswilson4502@outlook.com>
2020-10-21 21:16:21 +0100

    minimised the renderer function to one line (while drunk xD)

--- src/index.ts
+++ src/index.ts
@@ -65,13 +65,7 @@ export function renderFile(
 }
 
 export function render(window: Electron.BrowserWindow, rawEjs: string, data: oEjs.Data, options: oEjs.Options) {
-  if (typeof data === undefined || data === undefined || data == null) {
-    data = {};
-  }
-  if (typeof options === undefined || options === undefined || options == null || options === '') {
-    options = {};
-  }
-  window.loadURL('data:text/html;charset=UTF-8,' + encodeURIComponent(oEjs.render(rawEjs, data, options).toString()));
+  window.loadURL('data:text/html;charset=UTF-8,' + encodeURIComponent(oEjs.render(rawEjs, data || {}, options || {}).toString())); // this could be impoved to have the same buffer as the other one e.g. have an extra header as a true of false with the keyword file so that location.reload would work maybe idk im drunk XD
 }
 
 exports.cache = oEjs.cache;
